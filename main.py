462f998fbeac3cd0d52a8a82672a261f8931a03d
saikumar8639 <Gundeboinasaikumar86@gmail.com>
2024-11-08 06:11:15 +0530

    Basic Program to Render a basic page

--- main.py
+++ main.py
@@ -1,3 +1,16 @@
 # Creation of APIS using python
 # required packages : Flask
 # Developed by : Sai  Kumar
+
+#importing the required functions from Flask
+from flask import Flask,request,jsonify
+
+#Creating app of Flask 
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+    return "This is Home"
+#starting of Main 
+if __name__ == "__main__":
+    app.run(debug=True)
